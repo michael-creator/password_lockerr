7416db939611bae3ba74ee6986e43bd57d412cfd
michael <mikelkarije@gmail.com>
2019-11-20 10:22:48 +0300

    defined user blueprint and information of user

---
+++ user.py
@@ -0,0 +1,8 @@
+class USer:
+    user_list =[]
+    '''
+    this class defines blueprint of the user
+    '''
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
